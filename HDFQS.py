ca9f4f4b4822cdd4e48ccdce64fa9e8d622b64ff
Samuel Li <sam@projreality.com>
2014-12-22 18:58:15 -0800

    Remove unused initialize_file function.

--- HDFQS.py
+++ HDFQS.py
@@ -202,20 +202,3 @@ class HDFQS:
       elif (filename[-3:] == ".h5"):
         self.clean(full_path, min_time=min_time, index=index);
 
-################################################################################
-############################### INITIALIZE FILE ################################
-################################################################################
-  @staticmethod
-  def initialize_file(filename):
-    fd = openFile(filename, mode="w");
-    root = fd.root;
-
-    fd.createGroup(root, "Location");
-    fd.createGroup(root, "Health");
-    fd.createGroup(root, "Environment");
-    fd.createGroup(root, "Activity");
-    fd.createGroup(root, "Context");
-    fd.createGroup(root, "Sources");
-
-    fd.close();
-
