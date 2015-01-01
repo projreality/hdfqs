c68b44014d28591dba02f4894b4c5543b6a7f75a
Samuel Li <sam@projreality.com>
2014-12-31 20:04:33 -0800

    Add files for Python package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup;
+
+setup(name          = "hdfqs",
+      version       = "1.0.0",
+      py_modules    = [ "hdfqs" ],
+      author        = "Samuel Li",
+      author_email  = "sam@projreality.com",
+      url           = "http://www.projreality.com/hdfqs",
+      description   = "HDFQS Python Library",
+      license       = "https://www.gnu.org/licenses/lgpl.html"
+     );
