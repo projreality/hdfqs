2e778b1cda9bcaef0532392b8870080c97a8be03
Samuel Li <sam@projreality.com>
2015-01-06 21:44:30 -0800

    Add new copyright year.

--- hdfqs.py
+++ hdfqs.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Samuel Li
+# Copyright 2014, 2015 Samuel Li
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
